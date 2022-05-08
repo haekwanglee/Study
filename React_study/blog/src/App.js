d7b7369127c9ed85fdf1c12d44ff0bc2d03b6c38
haekwanglee <haekwangl@gmail.com>
2022-05-08 22:51:47 +0900

    Add modal map

--- React_study/blog/src/App.js
+++ React_study/blog/src/App.js
@@ -4,12 +4,10 @@ import { useState } from 'react';
 
 function App() {
 
-  let [modal, setModal] = useState(false);  
-  
-  let post = '테스트';
-  
-  let [App글제목, App글제목변경] = useState(['4번글', '2번글', '3번글']);  
-  let [좋아요개수, 좋아요개수변경] = useState(0);
+  let [App글제목, App글제목변경] = useState(['1번글', '2번글', '3번글']);  
+  let [좋아요개수, 좋아요개수변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false); 
+  let [modal글제목Index, modal글제목Index변경] = useState(0);
 
   function App글제목바꾸기함수(){    
     let copy = [...App글제목];
@@ -23,8 +21,15 @@ function App() {
     App글제목변경(newArray);
   }
 
-  function 모달창상태변경함수(){
-    setModal(!modal);
+  function 모달창상태변경함수(index){
+    modal글제목Index변경(index);
+    setModal(!modal);    
+  }
+
+  function 좋아요변경함수(i){
+    let copy = [...좋아요개수];
+    copy[i] = copy[i]+1;
+    좋아요개수변경(copy);
   }
 
   return (
@@ -32,24 +37,29 @@ function App() {
       <div className="black-nav">
         <h4>블로그임~~!</h4>
       </div>      
-      <h4>{ post }</h4>
       <h4> <button onClick={ App글제목바꾸기함수 }>App글제목 바꾸기 테스트버튼</button> </h4>
       <h4> <button onClick={ App글제목정렬하기함수 }>App글제목 정렬하기 테스트버튼</button> </h4>
-      <h4> <button onClick={ 모달창상태변경함수 }>모달창 상태변경 테스트버튼</button> </h4>
-      <div className="list">
-        <h4>{ App글제목[0] } <span onClick={ ()=>{ 좋아요개수변경(좋아요개수+1) } }>👍</span> { 좋아요개수 } </h4>
-        <p>4월 25일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{ App글제목[1] }</h4>
-        <p>4월 26일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{ App글제목[2] }</h4>
-        <p>4월 27일 발행</p>
-      </div>
+      <h4> <button onClick={ ()=>{모달창상태변경함수(0)} }>모달창 상태변경 테스트버튼</button> </h4>
       {
-        modal == true ? <Modal func={App글제목바꾸기함수} color={'skyblue'} title={App글제목}></Modal> : null
+        App글제목.map(function(글제목,i){
+          return (
+            <div className="list" key={i}>
+              <h4 onClick={()=>{모달창상태변경함수(i)}}>
+                { App글제목[i] } 
+                <button onClick={()=>{ 
+                  좋아요변경함수(i);
+                }}>👍</button> 
+                { 좋아요개수[i] } 
+              </h4>
+              <p>4월 25일 발행</p>
+            </div>
+          )
+        })
+      }
+      {
+        modal == true ? 
+        <Modal func={App글제목바꾸기함수} color={'skyblue'} title={App글제목} index={modal글제목Index}>          
+        </Modal> : null
       }      
     </div>
   );
@@ -58,7 +68,7 @@ function App() {
 function Modal(props) {
   return (    
     <div className="modal" style={{background : props.color}}>
-      <h4>{props.title[0]}</h4>
+      <h4>{props.title[props.index]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button onClick={props.func}>글수정</button>
